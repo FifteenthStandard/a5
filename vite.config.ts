ffc8a819bd557ba653837e9ac721e703cf386df9
Curtis Lusmore <curtis@fifteenthstandard.com>
2026-05-06 20:27:21 +1000

    Add GitHub Action for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE ?? '/',
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+  },
 });
